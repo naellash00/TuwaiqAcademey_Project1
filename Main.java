3f731ac7d62ae7f284c8c87f6ca38ed2b6ef171b
Naelah Alshibani <146096692+naellash00@users.noreply.github.com>
2024-11-02 17:57:02 +0300

    The 4th Version
    This version are just like the previous one, but it implements the 1-3 rounds of the game and check the winner

--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ public class Main {
 
         System.out.println("Please Enter Your Choice Of The Game (X or O): ");
         char userInput = input.next().charAt(0);
+        input.nextLine();
         char computerInput = ' ';
         if (userInput == 'X' || userInput == 'x') {
             computerInput = 'O';
@@ -14,72 +15,64 @@ public class Main {
         } else {
             System.out.println("Your Input Is Incorrect");
         }
-
-        char[][] theGameBoard = new char[3][3];
-        System.out.println("The Game Board: ");
-        printGameBoard(theGameBoard);
-
-        // THE SYSTEM VARIABLES:
-        int numberOfTries_forUserAndComputer = 0;
-        final int MAX_MOVES = 9;
-        // USER:
-        int selectPosition = 0; // the position the user want to enter their value at
-        int rowPosition_forUser = 0, columnPosition_forUser = 0;
-        // COMPUTER:
-        int range = (9 - 1) + 1; // the range for the random computer position
-        int computerRandomPosition = (int) (Math.random() * range) + 1;
-        int rowPosition_forComputer = 0, columnPosition_forComputer = 0;
-
-        //(7). complete checkWin method: DONE
-        while (numberOfTries_forUserAndComputer < MAX_MOVES) {
-            // user turn
-            do {
-                System.out.println("Choose available position from the board: ");
-                selectPosition = input.nextInt();
-                rowPosition_forUser = returnRowPosition(selectPosition);
-                columnPosition_forUser = returnColumnPosition(selectPosition);
-                // condition to check if position available or not
-                if (!isAvailablePosition(theGameBoard, rowPosition_forUser, columnPosition_forUser)) {
-                    System.out.println("The Position Is Unavailable, Try Again. ");
+        System.out.println("How Many Rounds You Want To Play? ");
+        int numberOfRounds = input.nextInt();
+        int numberOf_userWins = 0, numberOf_computerWins = 0;
+        boolean theWinner_isUser, theWinner_isComputer;
+        switch (numberOfRounds) {
+            case 1:
+                theWinner_isUser = playOneRound(input, userInput, computerInput);
+                if (theWinner_isUser) {
+                    numberOf_userWins++;
+                    System.out.println("Congrats, You Win The Game!");
+                } else {
+                    numberOf_computerWins++;
+                    System.out.println("Computer Wins The Game");
                 }
-            } while (!isAvailablePosition(theGameBoard, rowPosition_forUser, columnPosition_forUser));
-            // set the position after checking the available condition
-            theGameBoard[rowPosition_forUser][columnPosition_forUser] = userInput;
-            numberOfTries_forUserAndComputer++;
-
-            if (checkWin(theGameBoard, userInput)) {
-                System.out.println("Congrats You Win");
                 break;
-            }
-            if (numberOfTries_forUserAndComputer >= MAX_MOVES) {
-                System.out.println("There Is A Tie");
+            case 3:
+                for (int i = 1; i <= numberOfRounds; i++) {
+                    System.out.println("This Is Round " + i);
+                    theWinner_isUser = playOneRound(input, userInput, computerInput);
+
+                    // check who wins first round
+                    if (theWinner_isUser) {
+                        numberOf_userWins++;
+                        System.out.println("You Win This Round!");
+                    } else {
+                        numberOf_computerWins++;
+                        System.out.println("Computer Wins This Round");
+                    }
+
+                    //check who two rounds wins
+                    if (numberOf_userWins == 2) {
+                        System.out.println("Congrats, You Win The Game!");
+                        break;
+                    } else if (numberOf_computerWins == 2) {
+                        // used if-else so the below condition dont become unreachable
+                        System.out.println("Computer Wins The Game");
+                        break;
+                    }
+//                    else {
+//                        System.out.println("There Is A Tie");
+//
+//                    }
+
+                }
+                // check three rounds wins
+                // added the != 2 condition so it dosent print the win two times
+                if (numberOf_userWins > numberOf_computerWins && numberOf_userWins != 2) {
+                    System.out.println("Congrats, You Win The Game!");
+                } else if (numberOf_computerWins > numberOf_userWins && numberOf_computerWins != 2) {
+                    System.out.println("Computer Wins The Game");
+                }
+//                else {
+//                    System.out.println("There Is A Tie");
+//                }
                 break;
-            }
-            // computer round
-            do {
-                computerRandomPosition = (int) (Math.random() * range) + 1;
-                rowPosition_forComputer = returnRowPosition(computerRandomPosition);
-                columnPosition_forComputer = returnColumnPosition(computerRandomPosition);
-                // this loop will continue as long as the position is unavailable
-                // so it can randomly choose available one each time
-            } while (!isAvailablePosition(theGameBoard, rowPosition_forComputer, columnPosition_forComputer));
-
-            // now if the condition is available, put the value
-            theGameBoard[rowPosition_forComputer][columnPosition_forComputer] = computerInput;
-            numberOfTries_forUserAndComputer++;
-
-            if (checkWin(theGameBoard, computerInput)) {
-                System.out.println("Computer Wins");
-                break;
-            }
-
-            //print the game board
-            printGameBoard(theGameBoard);
-
+            default:
+                System.out.println("Incorrect Number, The Rounds Are Either One Or Three.");
         }
-        // this loop to print the board with both user and computer side
-        System.out.println("The Final Game Board");
-        printGameBoard(theGameBoard);
     }
 
     public static int returnRowPosition(int position) {
@@ -170,6 +163,78 @@ public class Main {
                 theUserAndComputerBoard[2][0] == XorO) {
             return true;
         }
+        return false;
+    }
+
+    public static boolean playOneRound(Scanner input, char userInput, char computerInput) {
+        char[][] theGameBoard = new char[3][3];
+        System.out.println("The Game Board: ");
+        printGameBoard(theGameBoard);
+
+        // THE SYSTEM VARIABLES:
+        int numberOfTries_forUserAndComputer = 0;
+        final int MAX_MOVES = 9;
+        // USER:
+        int selectPosition = 0; // the position the user want to enter their value at
+        int rowPosition_forUser = 0, columnPosition_forUser = 0;
+        // COMPUTER:
+        int range = (9 - 1) + 1; // the range for the random computer position
+        int computerRandomPosition = (int) (Math.random() * range) + 1;
+        int rowPosition_forComputer = 0, columnPosition_forComputer = 0;
+
+        //(7). complete checkWin method: DONE
+        while (numberOfTries_forUserAndComputer < MAX_MOVES) {
+            // user turn
+            do {
+                System.out.println("Choose available position from the board: ");
+                selectPosition = input.nextInt();
+                rowPosition_forUser = returnRowPosition(selectPosition);
+                columnPosition_forUser = returnColumnPosition(selectPosition);
+                // condition to check if position available or not
+                if (!isAvailablePosition(theGameBoard, rowPosition_forUser, columnPosition_forUser)) {
+                    System.out.println("The Position Is Unavailable, Try Again. ");
+                }
+            } while (!isAvailablePosition(theGameBoard, rowPosition_forUser, columnPosition_forUser));
+            // set the position after checking the available condition
+            theGameBoard[rowPosition_forUser][columnPosition_forUser] = userInput;
+            numberOfTries_forUserAndComputer++;
+
+            if (checkWin(theGameBoard, userInput)) {
+//                System.out.println("Congrats You Win");
+                // will return true if the user wins, false otherwise
+                return true;
+                //break;
+            }
+            if (numberOfTries_forUserAndComputer >= MAX_MOVES) {
+//                System.out.println("There Is A Tie");
+                return false;
+                // break;
+            }
+            // computer round
+            do {
+                computerRandomPosition = (int) (Math.random() * range) + 1;
+                rowPosition_forComputer = returnRowPosition(computerRandomPosition);
+                columnPosition_forComputer = returnColumnPosition(computerRandomPosition);
+                // this loop will continue as long as the position is unavailable
+                // so it can randomly choose available one each time
+            } while (!isAvailablePosition(theGameBoard, rowPosition_forComputer, columnPosition_forComputer));
+
+            // now if the condition is available, put the value
+            theGameBoard[rowPosition_forComputer][columnPosition_forComputer] = computerInput;
+            numberOfTries_forUserAndComputer++;
+
+            if (checkWin(theGameBoard, computerInput)) {
+//                System.out.println("Computer Wins");
+                return false;
+                // break;
+            }
+            //print the game board
+            printGameBoard(theGameBoard);
+        }
+        // this loop to print the board with both user and computer side
+        System.out.println("The Final Game Board");
+        printGameBoard(theGameBoard);
+
         return false;
     }
 
