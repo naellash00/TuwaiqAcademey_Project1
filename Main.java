cc1abbd6c964b431bdfd5598bdf0be31112606df
Naelah Alshibani <146096692+naellash00@users.noreply.github.com>
2024-11-02 12:39:37 +0300

    the second version
    this code has these tasks:
    -combines both user and computer values in the same board
    -check if the position is available or not
    -correctly print the boards with numbers at the available position 
    -correctly alternate between the user and computer each round

--- Main.java
+++ Main.java
@@ -1,141 +1,225 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         // to give the user and computer a specific input (X or O)
-        System.out.println("Please Enter Your Choice (X or O): ");
+        System.out.println("Please Enter Your Choice of the game (X or O): ");
         char userInput = input.next().charAt(0);
         char computerInput = ' ';
-        if(userInput=='X' || userInput == 'x'){
+        if (userInput == 'X' || userInput == 'x') {
             computerInput = 'O';
-        } else if(userInput == 'O' || userInput == 'o'){
+        } else if (userInput == 'O' || userInput == 'o') {
             computerInput = 'X';
         } else {
             System.out.println("Your Input Is Incorrect");
         }
 
-        //(1). Showcase the 9 positions of the tic tac toc:
-        int[][] allThePositions = {{1, 2, 3},
+        int[][] theGameBoard = {{1, 2, 3},
                 {4, 5, 6},
                 {7, 8, 9}};
-        // initial loop to just print the board
-        for (int i = 0; i < allThePositions.length; i++) {
-            for (int j = 0; j < allThePositions[i].length; j++) {
-                System.out.print(allThePositions[i][j] + " ");
+        //  loop to just print the board
+        System.out.println("The Game Board: ");
+        for (int i = 0; i < theGameBoard.length; i++) {
+            for (int j = 0; j < theGameBoard[i].length; j++) {
+                System.out.print(theGameBoard[i][j] + " ");
             }
             System.out.println(); // to print a new line between the rows
         }
 
-        // (2). I want the user to enter a value at an index they choose: DONE
-        // --> next step is to check if available position or not
         // THE USER VARIABLES
-        int numberOfTriesForUser = 0; // The loop counter for how many times user enter values at the board
         int selectPosition = 0; // the position the user want to enter their value at
-        int rowPosition = 0, columnPosition = 0;
-        char[][] userInputs = new char[3][3];
-
+        int rowPosition_forUser = 0, columnPosition_forUser = 0;
+        int numberOfTriesForUser = 0; // The loop counter for how many times user enter values at the board
         // THE COMPUTER VARIABLES
         int range = (9 - 1) + 1; // the range for the random computer position
         int computerRandomPosition = (int) (Math.random() * range) + 1;
-        int rowPosition_forComputer = returnRowPosition(computerRandomPosition);
-        int columnPosition_forComputer = returnColumnPosition(computerRandomPosition);
-        char[][] computerInputs = new char[3][3];
-        int computerNumberOfTries = 0;
-
-        //INCOMPLETE:
-        boolean isAvailablePosition = true;
+        int rowPosition_forComputer = 0, columnPosition_forComputer = 0;
+        int numberOfTriesForComputer = 0;
+        //System variables:
         // this 2d array to store at it BOTH computer and user values
         char[][] theBoard_forUserAndComputer = new char[3][3];
+        int numberOfTries_forUserAndComputer = 0; // numberOfTriesForUser + numberOfTriesForComputer;
+        final int MAX_MOVES = 9;
 
-        // THE USER POSITIONS LOOP
-        while(numberOfTriesForUser < allThePositions.length){
-            System.out.println("Choose available position from the board: ");
-            selectPosition = input.nextInt();
-            rowPosition = returnRowPosition(selectPosition);
-            columnPosition = returnColumnPosition(selectPosition);
+//        // working on combining user and computer inputs in one board: DONE
+//        // USER SIDE:
+//        // current problem --> the user enter more than 9 times not 3-4 times
+//        // --> separate them and fix the available position issue: DONE
+//        while (numberOfTriesForUser < 4) {
+//            do {
+//                System.out.println("Choose available position from the board: ");
+//                selectPosition = input.nextInt();
+//                rowPosition_forUser = returnRowPosition(selectPosition);
+//                columnPosition_forUser = returnColumnPosition(selectPosition);
+//                // condition to check each time if position available or not
+//                if (!isAvailablePosition(theBoard_forUserAndComputer, rowPosition_forUser, columnPosition_forUser)) {
+//                    System.out.println("The position is unavailable. try again");
+//                }
+//            } while (!isAvailablePosition(theBoard_forUserAndComputer, rowPosition_forUser, columnPosition_forUser));
+//
+//            // set the position after checking the available condition
+//            theBoard_forUserAndComputer[rowPosition_forUser][columnPosition_forUser] = userInput;
+//
+//            // here print the board with each update
+//            for (int i = 0; i < theBoard_forUserAndComputer.length; i++) {
+//                for (int j = 0; j < theBoard_forUserAndComputer[i].length; j++) {
+//                    System.out.print(theBoard_forUserAndComputer[i][j]);
+//
+//                }
+//                System.out.println();
+//            }
+//
+//            numberOfTriesForUser++;
+//        }
+//
+//        // COMPUTER SIDE:
+//        // Trying the computer side with different condition in the while loop
+//        while (numberOfTriesForComputer < 4) {
+//            do {
+//                computerRandomPosition = (int) (Math.random() * range) + 1;
+//                rowPosition_forComputer = returnRowPosition(computerRandomPosition);
+//                columnPosition_forComputer = returnColumnPosition(computerRandomPosition);
+//                // this loop will continue as long as the position is unavailable
+//                // so it can randomly choose available one each time
+//            } while (!isAvailablePosition(theBoard_forUserAndComputer, rowPosition_forComputer, columnPosition_forComputer));
+//
+//            // now if the condition is available, put the value
+//            theBoard_forUserAndComputer[rowPosition_forComputer][columnPosition_forComputer] = computerInput;
+//
+//            // print the board
+//            for (int i = 0; i < theBoard_forUserAndComputer.length; i++) {
+//                for (int j = 0; j < theBoard_forUserAndComputer[i].length; j++) {
+//                    System.out.print(theBoard_forUserAndComputer[i][j]);
+//
+//                }
+//                System.out.println();
+//            }
+//            numberOfTriesForComputer++;
+//        }
+//
+//
 
-            // removing these because the user input is selected before
-//            System.out.println("Enter " + userInput);
-//            userInput = input.next().charAt(0);
-            for(int i = 0; i < userInputs.length; i++) {
-                userInputs[rowPosition][columnPosition] = userInput;
+
+        //(3). fix the previous code to check if the position is available or not: DONE
+        //(5). print the updated board each time the user and computer enter something: DONE
+        //(6). make the user and computer enter values one after the other: DONE
+        //(7). complete checkWin method and check for exceptions
+        while (numberOfTries_forUserAndComputer < MAX_MOVES) {
+            // user turn
+            do {
+                System.out.println("Choose available position from the board: ");
+                selectPosition = input.nextInt();
+                rowPosition_forUser = returnRowPosition(selectPosition);
+                columnPosition_forUser = returnColumnPosition(selectPosition);
+                // condition to check each time if position available or not
+                if (!isAvailablePosition(theBoard_forUserAndComputer, rowPosition_forUser, columnPosition_forUser)) {
+                    System.out.println("The position is unavailbale. try again");
+                }
+            } while (!isAvailablePosition(theBoard_forUserAndComputer, rowPosition_forUser, columnPosition_forUser));
+            // set the position after checking the available condition
+            theBoard_forUserAndComputer[rowPosition_forUser][columnPosition_forUser] = userInput;
+            numberOfTries_forUserAndComputer++;
+            if (checkWin()) {
+                System.out.println("you win");
+                break;
             }
-            // (6). HERE it should print the updated board each time
-
-            numberOfTriesForUser++;
-        }
-        // WORKING ON THE COMPUTER INPUT -- the computer positions loop
-        while (computerNumberOfTries < 4) {
-            // this three lines to give the computer new position everytime
-            computerRandomPosition = (int) (Math.random() * range) + 1;
-            rowPosition_forComputer = returnRowPosition(computerRandomPosition);
-            columnPosition_forComputer = returnColumnPosition(computerRandomPosition);
-            for (int i = 0; i < computerInputs.length; i++) {
-                computerInputs[rowPosition_forComputer][columnPosition_forComputer] = computerInput;
+            if (numberOfTries_forUserAndComputer >= MAX_MOVES) {
+                System.out.println("tie");
+                break;
             }
-            computerNumberOfTries++;
+
+            // computer round
+            do {
+                computerRandomPosition = (int) (Math.random() * range) + 1;
+                rowPosition_forComputer = returnRowPosition(computerRandomPosition);
+                columnPosition_forComputer = returnColumnPosition(computerRandomPosition);
+                // this loop will continue as long as the position is unavailable
+                // so it can randomly choose available one each time
+            } while (!isAvailablePosition(theBoard_forUserAndComputer, rowPosition_forComputer, columnPosition_forComputer));
+
+            // now if the condition is available, put the value
+            theBoard_forUserAndComputer[rowPosition_forComputer][columnPosition_forComputer] = computerInput;
+            numberOfTries_forUserAndComputer++;
+
+            if (checkWin()) {
+                System.out.println("computer wins");
+                break;
+            }
+
+            //print the game board
+            printGameBoard(theBoard_forUserAndComputer);
+
         }
-        // this loop just to print the user inputs
-        for (int i = 0; i < userInputs.length; i++) {
-            for (int j = 0; j < userInputs[i].length; j++) {
-                System.out.print(userInputs[i][j]);
+        // this loop to print the board with both user and computer side
+        System.out.println("The final board");
+        for (int i = 0; i < theBoard_forUserAndComputer.length; i++) {
+            for (int j = 0; j < theBoard_forUserAndComputer[i].length; j++) {
+                System.out.print(theBoard_forUserAndComputer[i][j] + " ");
             }
             System.out.println();
         }
-        // This loop is just to print the computer positions
-        for (int i = 0; i < computerInputs.length; i++) {
-            for (int j = 0; j < computerInputs[i].length; j++) {
-                System.out.print(computerInputs[i][j] + " ");
-            }
-            System.out.println();
-        }
-
-        // working on combining user and computer inputs in one board
-
-
-
-        //(3). I want to fix the previous code to check if the position is available or not
-        //(4). I want the computer to select its random position too and enter its value: DONE
-        // (5). I want to print the updated board each time the user and computer enter something
-
 
     }
 
-    public static int returnRowPosition(int userSelectedPosition) {
+    public static int returnRowPosition(int position) {
         int rowNumber = 0;
-        if (userSelectedPosition == 1 || userSelectedPosition == 2 || userSelectedPosition == 3) {
+        if (position == 1 || position == 2 || position == 3) {
             rowNumber = 0;
-        } else if (userSelectedPosition == 4 || userSelectedPosition == 5 || userSelectedPosition == 6) {
+        } else if (position == 4 || position == 5 || position == 6) {
             rowNumber = 1;
-        } else if (userSelectedPosition == 7 || userSelectedPosition == 8 || userSelectedPosition == 9) {
+        } else if (position == 7 || position == 8 || position == 9) {
             rowNumber = 2;
         } else
             System.out.println("your number is incorrect");
         return rowNumber;
     }
 
-    public static int returnColumnPosition(int userSelectedPosition) {
+    public static int returnColumnPosition(int position) {
         int columnNumber = 0;
-        if (userSelectedPosition == 1 || userSelectedPosition == 4 || userSelectedPosition == 7) {
+        if (position == 1 || position == 4 || position == 7) {
             columnNumber = 0;
-        } else if (userSelectedPosition == 2 || userSelectedPosition == 5 || userSelectedPosition == 8) {
+        } else if (position == 2 || position == 5 || position == 8) {
             columnNumber = 1;
-        } else if (userSelectedPosition == 3 || userSelectedPosition == 6 || userSelectedPosition == 9) {
+        } else if (position == 3 || position == 6 || position == 9) {
             columnNumber = 2;
         } else
             System.out.println("your number is incorrect");
         return columnNumber;
     }
 
-    public static boolean isAvailablePosition(char[][] array, int rownum, int columnNum) {
-        //currently the array is alla digit thats why its always false
+    public static boolean isAvailablePosition(char[][] array, int rowNumber, int columnNumber) {
         boolean isAvailable = false;
-        if (Character.isDigit(array[rownum][columnNum])) {
+        if (array[rowNumber][columnNumber] == '\u0000') {
             isAvailable = true;
         }
         return isAvailable;
     }
+
+    public static void printGameBoard(char[][] theUserAndComputerBoard) {
+        int positionNumber = 1; // from 1 to 9
+        // PROBLEM !! now its only printing the board with empty char values and
+        // i want it to print it with numbers values: DONE
+        for (int i = 0; i < theUserAndComputerBoard.length; i++) {
+            for (int j = 0; j < theUserAndComputerBoard[i].length; j++) {
+                //System.out.print(theUserAndComputerBoard[i][j] + " ");
+                //check if the char position is empty --> print the corresponding number in the board
+                if (theUserAndComputerBoard[i][j] == '\u0000') {
+                    System.out.print(positionNumber + " ");
+                } else { // and if its unavailable position --> print the char value (x or o)
+                    System.out.print(theUserAndComputerBoard[i][j] + " ");
+                }
+                positionNumber++; // to move it forward each time
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static boolean checkWin() {
+        System.out.println("congrats");
+
+        return false;
+    }
+
 }
